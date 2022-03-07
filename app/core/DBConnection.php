016840f3657f001012925b5497cd1441509ac9d2
Michel Paquette <paquettm@vaniercollege.qc.ca>
2022-03-07 12:40:59 -0500

    Issue 1: Load credentials from '.env'.

--- app/core/DBConnection.php
+++ app/core/DBConnection.php
@@ -7,10 +7,13 @@ class DBConnection{
 
 	private function __construct(){
 		if(self::$connection == null){
-			$host = 'localhost';
-			$DBname = 'myapplication';
-			$user = 'root';
-			$password = '';
+			//fetch environment variables from .env
+			$dotenv = \Dotenv\Dotenv::createImmutable(getcwd());
+			$dotenv->load();
+			$host = $_ENV['db_host'];
+			$DBname = $_ENV['db_name'];
+			$user = $_ENV['db_user'];
+			$password = $_ENV['db_pass'];
 			self::$connection = new \PDO("mysql:host=$host;dbname=$DBname", $user, $password);
 		}
 	}
--- app/core/init.php
+++ app/core/init.php
@@ -2,3 +2,4 @@
 	//TODO: more initialization goes in this file
 	session_start();
 	require_once('app/core/autoload.php');
+	require_once('vendor/autoload.php');
