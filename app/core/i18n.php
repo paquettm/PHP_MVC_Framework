0c701d92e633412690254e0395bb067cca157dd0
Michel Paquette <mic.paquette@gmail.com>
2022-04-03 17:17:04 -0400

    Moved i18n language to session variables.

--- app/core/i18n.php
+++ app/core/i18n.php
@@ -1,19 +1,20 @@
 <?php
-$localizations = ['en', 'fr_CA'];
+$localizations = ['en', 'fr_CA', 'ar'];
 
 //to accept languages from the querystring as follows: mysite.com?lang=fr_CA
 if(isset($_GET['lang'])){ //if there is a language choice in the querystring
 	$lang = $_GET['lang'];//use this language
-}elseif (isset($_COOKIE["lang"])) {//is a cookie was set
-	$lang = $_COOKIE["lang"];//use this language
+}elseif (isset($_SESSION["lang"])) {//is a cookie was set
+	$lang = $_SESSION["lang"];//use this language
 }else{//if the browser is requesting a language
 	$lang = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);//use this language
 }
 
 $lang = Locale::lookup($localizations, $lang, true, 'en');
 
-setcookie("lang",$_COOKIE['lang'],1,"/","",true,true); //set the old cookie to expire
-setcookie("lang",$lang,0,"/","",true,true); //set a cookie
+//setcookie("lang",$_COOKIE['lang'],1,"/","",true,true); //set the old cookie to expire
+//setcookie("lang",$lang,0,"/","",true,true); //set a cookie
+$_SESSION['lang'] = $lang;
 
 //extract the root language from the complete locale to use with strftime
 $rootlang = preg_split('/_/', $lang); $rootlang = (is_array($rootlang)?$rootlang[0]:$rootlang);
