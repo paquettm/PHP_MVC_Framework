fe03306ab48b63e01a43e0e66a6b3895ce670dc4
Michel Paquette <paquettm@vaniercollege.qc.ca>
2022-11-11 10:57:58 -0500

    correction

--- app/controllers/Test.php
+++ app/controllers/Test.php
@@ -9,10 +9,10 @@ class Test extends \app\core\Controller{
 
 	function feedback(){
 		foreach ($_GET as $key=>$value){
-			echo "GET: $key => $value";
+			echo "GET: $key => $value\n";
 		}
 		foreach ($_POST as $key=>$value){
-			echo "GET: $key => $value";
+			echo "POST: $key => $value\n";
 		}
 	}
 }
