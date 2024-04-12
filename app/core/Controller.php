f9ce0baa7b70a2b1bb501e7549ed64be6518444b
Michel Paquette <mic.paquette@gmail.com>
2024-04-12 09:47:19 -0400

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,6 +3,9 @@ namespace app\core;
 
 class Controller{
 	public function view($name, $data = []){
+		if(is_array($data) && !array_is_list($data)){
+			extract($data);
+		}
 		if(file_exists('app/views/' . $name . '.php')){
 			global $localizations;
 			global $lang;
